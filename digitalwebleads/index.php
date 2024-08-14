6ca09a1d0aebf90fc6b2eb60d188f82f43c122ce
zwill <zwill_1@hotmail.com>
2024-08-13 23:20:09 -0600

    adding footer and header to file

--- digitalwebleads/index.php
+++ digitalwebleads/index.php
@@ -1,1 +1,13 @@
-<p>Theme loaded</p>
+<?php get_header(); ?>
+
+<?php 
+
+while(have_posts()){
+    the_post() ?>
+    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+    <p><?php the_content(); ?></p>
+<?php } 
+?>
+
+
+<?php get_footer(); ?>
